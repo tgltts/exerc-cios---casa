38b1f931b168cb80456030f76ca211b857cf767e
tgltts <g82651504@gmail.com>
2025-08-19 15:54:58 -0300

    Create soma.c

---
+++ soma.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    float a; 
+    float b; 
+    float soma;
+  
+    printf("digite o número : "); 
+    scanf("%f", &a); 
+  
+    printf("digite o número : "); 
+    scanf("%f", &b); 
+
+    soma = a + b; 
+    printf("Soma = %f\n", soma);  // 
+
+    return 0;
